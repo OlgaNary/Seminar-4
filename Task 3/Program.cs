202016a2f7c09ddb4b23e2035ebebd6ce4122866
Ольга <olga@mail.ru>
2023-01-27 09:43:47 +0300

    Task3

---
+++ Task 3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+//Вариант метода, когда массив задается рандомно:
+
+// int[] GetArray(int lengthArr)
+// {
+//     int[] array = new int[lengthArr];
+//     for (int i = 1; i < lengthArr; i++)
+//     {
+//         array[i] = new Random().Next(0, 10);
+//     }
+//     return array;
+// }
+
+//Вариант, когда массив вводит пользователь
+
+PrintArray(GetArray(8));
+
+int[] GetArray(int lengthArr)
+{
+    int[] array = new int[lengthArr];
+    for (int i = 1; i < lengthArr; i++)
+    {
+        System.Console.WriteLine("Введите число массива");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    System.Console.Write("Вы ввели массив: ");
+    System.Console.Write("[");
+    foreach (int item in arr)
+    {
+        Console.Write(item + " ");
+    }
+    System.Console.WriteLine("]");
+}
